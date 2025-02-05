ca2410858e7222e45c33d4cf201d85209287642f
DUDEamee <ameenameen8701@gmail.com>
2025-02-05 15:58:08 +0530

    java : pattern_2 completed with array

--- pattern1.java
+++ pattern1.java
@@ -4,7 +4,7 @@ public class pattern1 {
 
         
 
-        String[] array = {"A","B","C","D","E"};
+        String[] array = {"A","B","C","D","E"}; 
 
         for(int i=0; i <= array.length;i++){
             for(int j=0;j<=i;j++){
