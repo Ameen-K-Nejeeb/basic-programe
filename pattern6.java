6c4748498410332751a4a6000938c7f3ee4bb912
DUDEamee <ameenameen8701@gmail.com>
2025-02-05 18:32:30 +0530

    java : pattern_5 completed

---
+++ pattern6.java
@@ -0,0 +1,21 @@
+
+
+public class pattern6 {
+    
+    public static void main(String arg[]){
+
+        int num=1;
+
+
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(num+" ");
+                num++;
+            }System.out.print("\n");
+
+        }
+        
+
+    }
+}
+
