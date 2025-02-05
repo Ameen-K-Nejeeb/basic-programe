75862fcd49174bf7699d5606ce4e0aa8c428672c
DUDEamee <ameenameen8701@gmail.com>
2025-02-05 16:05:00 +0530

    java : pattern_3 completed

--- pattern1.java
+++ pattern1.java
@@ -2,16 +2,13 @@ public class pattern1 {
     
     public static void main(String arg[]){
 
-        
-
-        String[] array = {"A","B","C","D","E"}; 
-
-        for(int i=0; i <= array.length;i++){
+        for(int i=5;i>=0;i--){
             for(int j=0;j<=i;j++){
-                System.out.print(array[i]+" ");  
+                System.out.print("*"+" ");
             }
-            System.out.println("\n");
+            System.out.println(" ");
         }
+
 
     }
 
