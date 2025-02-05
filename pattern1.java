6d717a38b94510d0d269cec33065e79828b0a4f7
DUDEamee <ameenameen8701@gmail.com>
2025-02-05 16:06:08 +0530

    java : pattern_4 completed

--- pattern1.java
+++ pattern1.java
@@ -3,8 +3,8 @@ public class pattern1 {
     public static void main(String arg[]){
 
         for(int i=5;i>=0;i--){
-            for(int j=0;j<=i;j++){
-                System.out.print("*"+" ");
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
             }
             System.out.println(" ");
         }
