d85a816decaafd4001e3c9d64ea87bf772f9f80c
DUDEamee <ameenameen8701@gmail.com>
2025-02-07 11:22:00 +0530

    java : get array limit and value from user "sorting"

--- searching.java
+++ searching.java
@@ -4,56 +4,53 @@ import java.util.Scanner;
 
 
 
+
+
 public class searching {
 
     public static void main(String arg[]){
 
-        //searching a value in array
-        System.out.println("enter array limit :");
+      //programe to sorting
 
-        @SuppressWarnings("resource")
-        Scanner limi=new Scanner(System.in);
-        int limit = limi.nextInt();   
-        
-        int array[]=new int[limit];
-        
-        System.out.println("Enter array values");
-        
-           
+      System.err.println("enter array limit :");
 
-        for(int i=0;i<limit;i++){
-             array[i] = limi.nextInt();
-        }
+    Scanner n=new Scanner(System.in);
+    int limit=n.nextInt();
 
-        System.out.print("Entered value is :");
-        for(int i=0;i<limit;i++){
-            System.out.print(array[i]+",");
-       }
-        
-       System.out.print("\n Pleace enter your search key :");
+    int array[] = new int[limit];
 
-       Scanner scanner = new Scanner(System.in);
-       int search = scanner.nextInt();
-       int flag=0;
+    System.out.println("enter array values :");
 
-       for(int i=0;i<limit;i++){
-            if(array[i]==search){
-                System.out.print("search key found at position :"+(i+1)+"\n");
-                flag =1;
-                break;
+    for (int i = 0; i < limit; i++) {
+        array[i]=n.nextInt();
+    }
+    System.out.print("before sorting\n");
+    for(int i=0;i<limit;i++){
+        System.out.print(array[i]+",");
+    }
+    System.out.println("\n");
+
+
+
+
+
+    for(int i=0;i<limit-1;i++){
+        for(int j=i+1;j<limit;j++){
+            if(array[i]>array[j]){
+                int temp;
+                temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
             }
-          }
-          
-          if(flag==0){
-            System.out.println("search key not found !");
-            
+        }
+    }
+    System.out.print("\nater sorting :\n");
 
-       }
-       
-        
-        
+    for(int i=0;i<limit;i++){
+        System.out.print(array[i]+",");
+    }
 
-       
+
 
 
     }
