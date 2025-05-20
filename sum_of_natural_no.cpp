b2da195d4c16c17f67370b1d4a2e0060b8bb71b4
DUDEamee <ameenameen8701@gmail.com>
2025-05-20 21:08:00 +0530

    cpp : searching

--- sum_of_natural_no.cpp
+++ sum_of_natural_no.cpp
@@ -1,14 +1,30 @@
 #include<stdio.h>
 int main(){
-    int n,sum,i;
+      
+    int i,searchkey,limit,values[100], flag = 0;
+    printf("Enter array limit :");
+    scanf("%d",&limit);
 
-    printf("Enter a number : ");
-    scanf("%d",&n);
-
-    for(i=0;i<=n;i++){
-        sum += i;
+    printf("Enter array values");
+    for(i=0;i<=limit;i++){
+        scanf("%d",&values[i]);
     }
-    printf("Sum of the first %d natural numbers is: %d\n", n, sum);
 
+
+    printf("Enter a number to check the number is available in that array : ");
+    scanf("%d",&searchkey);
+
+    for(i=0;i<limit;i++){
+        if(searchkey == values[i]){
+            flag = 1;
+            break;
+        }
+    }
+
+    if(flag == 1){
+        printf("searchkey found at position :%d",i+1);
+    }else{
+        printf("Not found searchkey");
+    }
 
 }
