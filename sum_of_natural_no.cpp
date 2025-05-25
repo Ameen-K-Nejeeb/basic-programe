9230865cc153a3f745b720663492fa94a42cdb51
DUDEamee <ameenameen8701@gmail.com>
2025-05-25 10:43:26 +0530

    check : palidrome or not

--- sum_of_natural_no.cpp
+++ sum_of_natural_no.cpp
@@ -1,30 +1,29 @@
-#include<stdio.h>
-int main(){
-      
-    int i,searchkey,limit,values[100], flag = 0;
-    printf("Enter array limit :");
-    scanf("%d",&limit);
 
-    printf("Enter array values");
-    for(i=0;i<=limit;i++){
-        scanf("%d",&values[i]);
+#include <stdio.h>
+
+
+//Write a program to check if a given number or string is a palindrome.
+
+int main() {
+
+    int number,reverced,orginal,reminder;
+
+    printf("Enter a number to check it is palidrome :");
+    scanf("%d",&number);
+
+    orginal = number;
+
+    while(number != 0){
+        reminder = number%10;
+        reverced = reverced *10 + reminder;
+        number = number/10;
+
     }
-
-
-    printf("Enter a number to check the number is available in that array : ");
-    scanf("%d",&searchkey);
-
-    for(i=0;i<limit;i++){
-        if(searchkey == values[i]){
-            flag = 1;
-            break;
-        }
-    }
-
-    if(flag == 1){
-        printf("searchkey found at position :%d",i+1);
+    if(orginal == reverced){
+        printf("Entered number is palidrome :%d \n",orginal);
     }else{
-        printf("Not found searchkey");
+        printf("Entered number is not palidrome :%d \n",orginal);
     }
 
+    return 0;
 }
