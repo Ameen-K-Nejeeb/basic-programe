af56aa7d8babf6e7341d88c2d256cdff3c5ea2ca
DUDEamee <ameenameen8701@gmail.com>
2025-06-02 14:45:48 +0530

    merge two sorted arrays into one sorted array in C

--- sample.c
+++ sample.c
@@ -1,21 +1,43 @@
 #include <stdio.h>
+//How to merge two sorted arrays into one sorted array in C?
+
+
 
 int main() {
     
-    int i,j;
-    int arr1[] = {1,2,3,4,5,6};
-    int arr2[] = {10,20,50,40,30};
+    int i,j,temp;
+    int arr1[] = {1,8,3,2,5,7};
+    int arr2[] = {10,60,50,72,30};
     int merge[50];
     
     int size1 = sizeof(arr1)/sizeof(arr1[0]);
-    int size2 = sizeof(arr2)/sizeof(arr1[0]);
+    int size2 = sizeof(arr2)/sizeof(arr2[0]);
     
-    printf("\n__Array1 value__\n");
+    for(i=0;i<size1-1;i++){
+        for(j=0;j<=size1-i-1;j++){
+            if(arr1[j]>arr1[j+1]){
+                temp = arr1[j];
+                arr1[j] = arr1[j+1];
+                arr1[j+1] = temp;
+            }
+        }
+    }
+    
+    printf("\n__Sorted Array1 value__\n");
     for(i=0;i<size1;i++){
         printf("%d",arr1[i]);
         if( i!= size1-1)printf(", ");
     }
-    printf("\n__Array2 value__\n");
+    for(i=0;i<size2-1;i++){
+        for(j=0;j<size2-i-1;j++){
+            if(arr2[j]>arr2[j+1]){
+                temp = arr2[j];
+                arr2[j] = arr2[j+1];
+                arr2[j+1] = temp;
+            }
+        }
+    }
+    printf("\n__Sorted Array2 value__\n");
     for(i=0;i<size2;i++){
         printf("%d",arr2[i]);
         if( i!= size2-1)printf(", ");
@@ -30,6 +52,15 @@ int main() {
         merge[i+j] = arr2[j];
     }
     printf("\n");
+    for(i=0;i<size1+size2-1;i++){
+        for(j=0;j<size1+size2-i-1;j++){
+            if(merge[j]>merge[j+1]){
+                temp = merge[j];
+                merge[j] = merge[j+1];
+                merge[j+1] = temp;
+            }
+        }
+    }
     printf("\n__Merged Array__\n");
     for(i=0;i<size1+size2;i++){
         printf("%d",merge[i]);
