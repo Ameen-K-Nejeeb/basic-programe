20fdcfbab5d3eb60b433457c37fc4aa55c827aa0
DUDEamee <ameenameen8701@gmail.com>
2025-06-02 14:14:36 +0530

    program to merge two arrays into a single array

--- sample.c
+++ sample.c
@@ -2,33 +2,40 @@
 
 int main() {
     
-    int arr[] = {7,1,9,3,5};
-    int i , temp ;
+    int i,j;
+    int arr1[] = {1,2,3,4,5,6};
+    int arr2[] = {10,20,50,40,30};
+    int merge[50];
     
-    printf("Before sorting :");
-    for(i=0;i<5;i++){
-        printf("%d",arr[i]);
-        if(i != 4){
-            printf(", ");
-        }
-    }
-    for(int j =1;j<=5-1;j++){
-        for(i=0;i<5-1;i++){
-            if(arr[i]>arr[i+1]){
-                temp = arr[i];
-                arr[i] = arr[i+1];
-                arr[i+1] = temp;
-        }
-    }
-        
-    }
-    printf("\n After sorting :");
+    int size1 = sizeof(arr1)/sizeof(arr1[0]);
+    int size2 = sizeof(arr2)/sizeof(arr1[0]);
     
-    for(i=0;i<5;i++){
-        printf("%d",arr[i]);
-        if(i != 4){
-            printf(", ");
-        }
+    printf("\n__Array1 value__\n");
+    for(i=0;i<size1;i++){
+        printf("%d",arr1[i]);
+        if( i!= size1-1)printf(", ");
     }
+    printf("\n__Array2 value__\n");
+    for(i=0;i<size2;i++){
+        printf("%d",arr2[i]);
+        if( i!= size2-1)printf(", ");
+    }
+    
+    for(i=0;i<size1;i++){
+        merge[i] = arr1[i];
+    }
+    
+    
+    for(j=0;j<size2;j++){
+        merge[i+j] = arr2[j];
+    }
+    printf("\n");
+    printf("\n__Merged Array__\n");
+    for(i=0;i<size1+size2;i++){
+        printf("%d",merge[i]);
+        if( i!= size1+size2-1)printf(", ");
+    }
+    
+    
     return 0;
 }
