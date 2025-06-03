51668d7a3777c6a8514883ba6928c1b828977bf8
DUDEamee <ameenameen8701@gmail.com>
2025-06-03 08:56:20 +0530

    takes an array as input from the user, removes the even numbers from the array, and finds the average of the remaining values

--- sample.c
+++ sample.c
@@ -1,72 +1,43 @@
 #include <stdio.h>
-//How to merge two sorted arrays into one sorted array in C?
 
+// Write a program to get an array from users, remove even numbers from the array and find 6. the average of the existing values in the array.  
 
 
 int main() {
+    int limit,i,j=0;
+    float sum = 0;
+    int arr[100], oddno[50];
     
-    int i,j,temp;
-    int arr1[] = {1,8,3,2,5,7};
-    int arr2[] = {10,60,50,72,30};
-    int merge[50];
+    printf("Enter Array limit :");
+    scanf("%d",&limit);
     
-    int size1 = sizeof(arr1)/sizeof(arr1[0]);
-    int size2 = sizeof(arr2)/sizeof(arr2[0]);
-    
-    for(i=0;i<size1-1;i++){
-        for(j=0;j<=size1-i-1;j++){
-            if(arr1[j]>arr1[j+1]){
-                temp = arr1[j];
-                arr1[j] = arr1[j+1];
-                arr1[j+1] = temp;
-            }
+    printf("\nEnter array values :");
+    for(i=0;i<limit;i++){
+        scanf("%d",&arr[i]);
+    }
+    printf("\n Entered Array values are : ");
+    for(i=0;i<limit;i++){
+        printf("%d",arr[i]);
+        if(i!=limit-1)printf(", ");
+    }
+    int count=0;
+    for(i=0;i<limit;i++){
+        if(arr[i] % 2 != 0){
+            oddno[j++] = arr[i];
+            sum = sum + arr[i];
+            count++;
         }
     }
-    
-    printf("\n__Sorted Array1 value__\n");
-    for(i=0;i<size1;i++){
-        printf("%d",arr1[i]);
-        if( i!= size1-1)printf(", ");
+
+    printf("\n\n Odd no are :");
+    for(i=0;i<count;i++){
+        printf("%d",oddno[i]);
+        if(i!=count-1)printf(", ");
     }
-    for(i=0;i<size2-1;i++){
-        for(j=0;j<size2-i-1;j++){
-            if(arr2[j]>arr2[j+1]){
-                temp = arr2[j];
-                arr2[j] = arr2[j+1];
-                arr2[j+1] = temp;
-            }
-        }
-    }
-    printf("\n__Sorted Array2 value__\n");
-    for(i=0;i<size2;i++){
-        printf("%d",arr2[i]);
-        if( i!= size2-1)printf(", ");
-    }
-    
-    for(i=0;i<size1;i++){
-        merge[i] = arr1[i];
-    }
+    printf("\n Average of odd numbers :%f ",sum);
     
     
-    for(j=0;j<size2;j++){
-        merge[i+j] = arr2[j];
-    }
-    printf("\n");
-    for(i=0;i<size1+size2-1;i++){
-        for(j=0;j<size1+size2-i-1;j++){
-            if(merge[j]>merge[j+1]){
-                temp = merge[j];
-                merge[j] = merge[j+1];
-                merge[j+1] = temp;
-            }
-        }
-    }
-    printf("\n__Merged Array__\n");
-    for(i=0;i<size1+size2;i++){
-        printf("%d",merge[i]);
-        if( i!= size1+size2-1)printf(", ");
-    }
     
-    
+
     return 0;
 }
